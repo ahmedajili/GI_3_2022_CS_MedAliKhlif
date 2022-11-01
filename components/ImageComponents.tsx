bc82a02136606e9927020c65657c76c7745c9917
KhlifMedAli <khlifmedali10@gmail.com>
2022-11-01 21:31:02 +0100

    Troisiéme commit

--- components/ImageComponents.tsx
+++ components/ImageComponents.tsx
@@ -1,11 +1,12 @@
 import { Image, View } from "react-native";
 
 
-export default function ImageComponent() {
-        return (
-<View style={{ flex: 1, justifyContent: 'center' }}>
- <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://picsum.photos/100/100' }} />
+export default function ImageComponents() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://picsum.photos/100/100' }} />
+            <Image style={{ width: 100, height: 100 }} source={require('../assets/react-native.jpg')} />
 
- </View>
+        </View>
     );
 }
